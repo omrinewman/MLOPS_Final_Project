9800433470df601e6f9099c66a8db039b8357d24
itay shapira <itayshapira@hotmail.com>
2023-01-20 16:30:16 +0200

    Remove unnecacery packages and functions

--- predict.py
+++ predict.py
@@ -1,28 +1,7 @@
 import sys
-from xgboost import XGBRegressor
 import pandas as pd
-from sklearn.model_selection import train_test_split
 import pickle
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from Services.DataProcessingService import DataProcessingService
 
-def find_num_cat_features(df):
-    categorical_features = df.select_dtypes(include=['object']).columns.to_list()
-    numeric_features = df.columns.drop(categorical_features).to_list()
-    return categorical_features, numeric_features
-
-def create_pre_process_pipeline(categorical_features, numeric_features):
-    num_pipeline = Pipeline([('std_scaler', StandardScaler())])
-    preprocessor = ColumnTransformer(
-          transformers=[
-            ('categorical', OrdinalEncoder(), categorical_features)], remainder='passthrough')
-    return preprocessor
-
-def run_pre_process_pipeline(preprocessor, df, categorical_features, numeric_features):
-    processed_data = preprocessor.transform(df)
-    return pd.DataFrame(processed_data, columns=categorical_features +numeric_features)
 
 with open('good_model.pkl', 'rb') as fid:
     model = pickle.load(fid)
