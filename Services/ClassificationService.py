3e6ddec653ae984f11e3ace7624c4c52d3e4ebf6
itay shapira <itayshapira@hotmail.com>
2023-01-21 20:21:52 +0200

    removed the calssification service

--- Services/ClassificationService.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-class ClassificationService():
-    def __init__(self, classification_threshold) -> None:
-        self.classification_threshold = classification_threshold
-
-    def create_classification(self, target, pred) -> np.array:
-        return np.where(abs(pred - target) < self.classification_threshold + self.classification_threshold*target, True, False)
